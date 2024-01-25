ab02a230cc53d5559ad9b111c0307de24e7373d3
MuadzAlch <muadzchairi04@gmail.com>
2024-01-25 16:57:53 +0700

    fixed condition

--- include/condition.h
+++ include/condition.h
@@ -8,139 +8,202 @@ int main() {
 if (pesan == "activity"){
     const byte (*picture)[512] = activity;  
 }else if (pesan == "aircraft"){
+    const byte (*picture)[512] = aircraft;  
 
 }else if (pesan == "alarm_clock"){
+    const byte (*picture)[512] = alarm_clock;  
 
 }else if (pesan == "angel"){
+    const byte (*picture)[512] = angel;  
 
 }else if (pesan == "beating_hearts"){
+    const byte (*picture)[512] = beating_hearts;  
 
 }else if (pesan == "birthday_cake"){
+    const byte (*picture)[512] = birthday_cake;  
 
 }else if (pesan == "blushing"){
+    const byte (*picture)[512] = blushing;  
 
 }else if (pesan == "bow"){
-
-}else if (pesan == "bow"){
+    const byte (*picture)[512] = bow;  
 
 }else if (pesan == "broken_heart"){
+    const byte (*picture)[512] = broken_heart;  
 
 }else if (pesan == "car"){
+    const byte (*picture)[512] = car;  
 
 }else if (pesan == "cassette"){
+    const byte (*picture)[512] = cassette;  
 
 }else if (pesan == "cat_meow"){
+    const byte (*picture)[512] = cat_meow;  
 
 }else if (pesan == "confused"){
+    const byte (*picture)[512] = confused;  
 
 }else if (pesan == "cool"){
+    const byte (*picture)[512] = cool;  
 
 }else if (pesan == "crying"){
+    const byte (*picture)[512] = crying;  
 
 }else if (pesan == "customer_insight"){
+    const byte (*picture)[512] = customer_insight;  
 
 }else if (pesan == "day_night_weather"){
+    const byte (*picture)[512] = day_night_weather;  
 
 }else if (pesan == "deadlift"){
+    const byte (*picture)[512] = deadlift;  
 
 }else if (pesan == "design"){
+    const byte (*picture)[512] = design;  
 
 }else if (pesan == "disappointed"){
+    const byte (*picture)[512] = disappointed;  
 
 }else if (pesan == "embarrassed"){
+    const byte (*picture)[512] = embarrassed;  
 
 }else if (pesan == "energy_drink"){
+    const byte (*picture)[512] = energy_drink;  
 
 }else if (pesan == "eye"){
+    const byte (*picture)[512] = eye;  
 
 }else if (pesan == "face_id"){
+    const byte (*picture)[512] = face_id;  
 
 }else if (pesan == "family"){
+    const byte (*picture)[512] = family;  
 
 }else if (pesan == "fire_in_my_heart"){
+    const byte (*picture)[512] = fire_in_my_heart;  
 
 }else if (pesan == "flow"){
+    const byte (*picture)[512] = flow;  
 
 }else if (pesan == "fly_elephant"){
+    const byte (*picture)[512] = fly_elephant;  
 
 }else if (pesan == "ghost"){
+    const byte (*picture)[512] = ghost;  
 
 }else if (pesan == "gift_heart"){
+    const byte (*picture)[512] = gift_heart;  
 
 }else if (pesan == "handshake"){
+    const byte (*picture)[512] = handshake;  
 
 }else if (pesan == "heart"){
+    const byte (*picture)[512] = heart;  
 
 }else if (pesan == "heart_in_hand"){
+    const byte (*picture)[512] = heart_in_hand;  
 
 }else if (pesan == "heart_with_arrow"){
+    const byte (*picture)[512] = heart_with_arrow;  
 
 }else if (pesan == "home"){
+    const byte (*picture)[512] = home;  
 
 }else if (pesan == "in_love"){
+    const byte (*picture)[512] = in_love;  
 
 }else if (pesan == "kawaii"){
+    const byte (*picture)[512] = kawaii;  
 
 }else if (pesan == "kiss_panda"){
+    const byte (*picture)[512] = kiss_panda;  
 
 }else if (pesan == "laughing"){
+    const byte (*picture)[512] = laughing;  
 
 }else if (pesan == "letter_with_hearts"){
+    const byte (*picture)[512] = letter_with_hearts;  
 
 }else if (pesan == "lips"){
+    const byte (*picture)[512] = lips;  
 
 }else if (pesan == "maneki"){
+    const byte (*picture)[512] = maneki;  
 
 }else if (pesan == "mug"){
+    const byte (*picture)[512] = mug;  
 
 }else if (pesan == "nerd"){
+    const byte (*picture)[512] = nerd;  
 
 }else if (pesan == "ok_hand"){
+    const byte (*picture)[512] = ok_hand;  
 
 }else if (pesan == "open_sign"){
+    const byte (*picture)[512] = open_sign;  
 
 }else if (pesan == "page_under_construction"){
+    const byte (*picture)[512] = page_under_construction;  
 
 }else if (pesan == "palm_scan"){
+    const byte (*picture)[512] = palm_scan;  
 
 }else if (pesan == "piggy_bank"){
+    const byte (*picture)[512] = piggy_bank;  
 
 }else if (pesan == "poo"){
+    const byte (*picture)[512] = poo;  
 
 }else if (pesan == "puzzled"){
+    const byte (*picture)[512] = puzzled;  
 
 }else if (pesan == "qr_code"){
+    const byte (*picture)[512] = qr_code;  
 
 }else if (pesan == "reddit"){
+    const byte (*picture)[512] = reddit;  
 
 }else if (pesan == "rocket"){
+    const byte (*picture)[512] = rocket;  
 
 }else if (pesan == "shocker"){
+    const byte (*picture)[512] = shocker;  
 
 }else if (pesan == "silent"){
+    const byte (*picture)[512] = silent;  
 
 }else if (pesan == "sleeping"){
+    const byte (*picture)[512] = sleeping;  
 
 }else if (pesan == "tea"){
+    const byte (*picture)[512] = tea;  
 
 }else if (pesan == "tongue_out"){
+    const byte (*picture)[512] = tongue_out;  
 
 }else if (pesan == "torrential_rain_weather"){
+    const byte (*picture)[512] = torrential_rain_weather;  
 
 }else if (pesan == "vomited"){
+    const byte (*picture)[512] = vomited;  
 
 }else if (pesan == "walk"){
+    const byte (*picture)[512] = walk;  
 
 }else if (pesan == "wash_your_hands"){
+    const byte (*picture)[512] = wash_your_hands;  
 
 }else if (pesan == "wifi_search"){
+    const byte (*picture)[512] = wifi_search;  
 
 }else if (pesan == "wink"){
+    const byte (*picture)[512] = wink;  
 
 }else if (pesan == "yummy"){
+    const byte (*picture)[512] = yummy;  
 
 }
-
+return 0;
 }
 
 
